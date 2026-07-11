4e611ad49760f33c5492d791191a0580aa8bdb66
V4DYATV <79901874+V4DYA@users.noreply.github.com>
2026-07-11 12:54:11 +0300

    Create src/store/useMapUiStore.ts

---
+++ src/store/useMapUiStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+
+export type MapSheet = 'maps' | 'quests' | 'search' | 'layers';
+
+interface MapUiState {
+  activeSheet: MapSheet | null;
+  chromeCollapsed: boolean;
+  openSheet: (sheet: MapSheet) => void;
+  closeSheet: () => void;
+  toggleSheet: (sheet: MapSheet) => void;
+  toggleChrome: () => void;
+}
+
+export const useMapUiStore = create<MapUiState>((set) => ({
+  activeSheet: null,
+  chromeCollapsed: false,
+  openSheet: (sheet) => set({ activeSheet: sheet }),
+  closeSheet: () => set({ activeSheet: null }),
+  toggleSheet: (sheet) =>
+    set((s) => ({ activeSheet: s.activeSheet === sheet ? null : sheet })),
+  toggleChrome: () => set((s) => ({ chromeCollapsed: !s.chromeCollapsed })),
+}));
