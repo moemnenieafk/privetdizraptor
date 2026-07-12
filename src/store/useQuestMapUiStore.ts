b1c8545f99c87deb9e695a31909b213025ac9eb5
V4DYATV <79901874+V4DYA@users.noreply.github.com>
2026-07-12 06:03:10 +0300

    Create src/store/useQuestMapUiStore.ts

---
+++ src/store/useQuestMapUiStore.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+
+export type QuestSheet = 'search' | 'traders' | 'maps' | 'save';
+
+interface QuestMapUiState {
+  activeSheet: QuestSheet | null;
+  openSheet: (sheet: QuestSheet) => void;
+  closeSheet: () => void;
+  toggleSheet: (sheet: QuestSheet) => void;
+}
+
+export const useQuestMapUiStore = create<QuestMapUiState>((set) => ({
+  activeSheet: null,
+  openSheet: (sheet) => set({ activeSheet: sheet }),
+  closeSheet: () => set({ activeSheet: null }),
+  toggleSheet: (sheet) =>
+    set((s) => ({ activeSheet: s.activeSheet === sheet ? null : sheet })),
+}));
